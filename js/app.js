19d05db870d2ead9827023f45f033e630a4d8d68
fcastell-iesemili <fcp00@iesemilidarder.com>
2020-10-17 22:54:10 +0200

    Casi Acabat Pero button_search no funciona

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 import { Get_Film_01, Get_Film_02, Get_Film_03 } from './Get_Films.js';
+import { Get_Serie_01, Get_Serie_02, Get_Serie_03 } from './Get_Series.js';
+import { Search_Button_Terminator, Search_Button_Alien, Search_Button_Matrix } from './Search_Button.js';
+
+
 
 document.getElementById("first-button").addEventListener("click", function () {
     Get_Film_01();
     Get_Film_02();
     Get_Film_03();
+});
+
+document.getElementById("second-button").addEventListener("click", function () {
+    Get_Serie_01();
+    Get_Serie_02();
+    Get_Serie_03();
+});
+
+document.getElementById("search_button").addEventListener("click", function () {
+    Search_Button_Terminator();
+    Search_Button_Alien();
+    Search_Button_Matrix();
 });
