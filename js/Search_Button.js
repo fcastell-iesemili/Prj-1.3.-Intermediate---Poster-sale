e0aee0e5a9527e28a8fdb828854fa8bd23c32042
fcastell-iesemili <fcp00@iesemilidarder.com>
2020-10-20 17:47:47 +0200

    Pop-Up Arreglat

--- js/Search_Button.js
+++ js/Search_Button.js
@@ -1,131 +1,23 @@
-function Search_Button_Terminator() {
-    let button = document.getElementById('search_input').value;
-    nombre_peli = "terminator";
+function Search_Button() {
+    fetch('json/films.json')
+        .then(result => result.json())
+        .then(data => {
+            let txtSearch = document.getElementById("search_input").value;
+            txtSearch = txtSearch.toLowerCase();
 
-    if (button == nombre_peli) {
+            let listFilms = data.films.titulo;
 
-        fetch("json/films.json")
-            .then((result) => result.json())
-            .then((data) => {
-                let content = document.getElementById('Pelicula-1');
-                content.innerHTML = `
-            
-            <img src="${"img/photo-1.jpg"}" width="200px" height="300>
-
-            <div id="titles">
-                <h3>${data.films['0'].titulo}</h3>
-            </div>
-            
-            <p>Duraccion: ${data.films['0'].duracion}</p>
-            <p> Director: ${data.films['0'].director} </p>
-            <p> Genero: ${data.films['0'].genero} </p>
-            
-            <div id="titles"> 
-                <h4> Stars </h4>
-            </div>
-
-            <div class="something"> <p>${data.films['0'].stars['0']}</p> </div>
-            <div class="something"> <p>${data.films['0'].stars['1']}</p> </div>
-            <div class="something"> <p>${data.films['0'].stars['2']}</p> </div>
-
-            <button id="button_Terminator" type="button" class="btn btn-info btn-sm">Comprar</button>
-
-              `
-            });
-
-    }
-
-    else {
-
-        return true;
-    }
+            let titulo = listFilms.filter(
+                (item) =>
+                    txtSearch.toLowerCase() == item.name.toLowerCase()
+            );
+            if (titulo && titulo.length > 0) {
+                printFilms(titulo);
+            }
+            else {
+                return false;
+            }
+        })
 }
 
-
-function Search_Button_Alien() {
-    let button = document.getElementById('search_input').value;
-
-    nombre_peli = "alien";
-
-    if (button == nombre_peli) {
-
-        fetch("json/films.json")
-            .then((result) => result.json())
-            .then((data) => {
-                let content = document.getElementById('Pelicula-2');
-                content.innerHTML = `
-            <img src="${"img/photo-2.jpg"}" width="200px" height="300>
-
-            <div id="titles">
-                <h3>${data.films['1'].titulo}</h3>
-            </div>
-
-               <p>Duraccion: ${data.films['1'].duracion}</p>
-                <p> Director: ${data.films['1'].director} </p>
-                <p> Genero: ${data.films['1'].genero} </p>
-                
-                <div id="titles"> 
-                    <h4> Stars </h4>
-                </div>
-                
-                <p>${data.films['1'].stars['0']}</p>
-                <p>${data.films['1'].stars['1']}</p>
-                <p>${data.films['1'].stars['2']}</p>
-
-                <button id="button_Alien" type="button" class="btn btn-info btn-sm">Comprar</button>
-
-                `
-            });
-
-    }
-
-    else {
-
-        return true;
-    }
-}
-
-
-function Search_Button_Matrix() {
-    let button = document.getElementById('search_input').value;
-    nombre_peli = "matrix";
-
-    if (button == nombre_peli) {
-
-        fetch("json/films.json")
-            .then((result) => result.json())
-            .then((data) => {
-                let content = document.getElementById('Pelicula-3');
-                content.innerHTML = `
-            <img src="${"img/photo-3.jpg"}" width="200px" height="300>
-
-            <div id="titles">
-                <h3>${data.films['2'].titulo}</h3>
-            </div>
-
-               <p>Duraccion: ${data.films['2'].duracion}</p>
-                <p> Director: ${data.films['2'].director} </p>
-                <p> Genero: ${data.films['2'].genero} </p>
-                
-                <div id="titles"> 
-                    <h4> Stars </h4>
-                </div>
-                
-                <p>${data.films['2'].stars['0']}</p>
-                <p>${data.films['2'].stars['1']}</p>
-                <p>${data.films['2'].stars['2']}</p>
-
-                <button  id="button_Matrix" type="button" class="btn btn-info btn-sm">Comprar</button>
-              `
-            });
-
-    }
-
-    else {
-
-        return true;
-    }
-}
-
-
-export { Search_Button_Terminator, Search_Button_Alien, Search_Button_Matrix };
+export { Search_Button };
--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import { Get_Film_01, Get_Film_02, Get_Film_03 } from './Get_Films.js';
 import { Get_Serie_01, Get_Serie_02, Get_Serie_03 } from './Get_Series.js';
-import { Search_Button_Terminator, Search_Button_Alien, Search_Button_Matrix } from './Search_Button.js';
+import { Search_Button } from './Search_Button.js';
 
 
 document.getElementById("first-button").addEventListener("click", function () {
@@ -16,7 +16,5 @@ document.getElementById("second-button").addEventListener("click", function () {
 });
 
 document.getElementById("search_button").addEventListener("click", function () {
-    Search_Button_Terminator();
-    Search_Button_Alien();
-    Search_Button_Matrix();
+    Search_Button();
 });
